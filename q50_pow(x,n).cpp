64755eaf38b717e629d5199c3c9272fa5765dba6
krishna1911907 <krichauhan2005@gmail.com>
2025-07-17 22:50:13 +0530

    pow

---
+++ q50_pow(x,n).cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double a;
+        
+        a=pow(x,n);
+     
+        return a;
+    }
+};
